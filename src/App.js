cfd51e2e52bef568b1fbc12ce177ccc8f1d8f9cb
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-04-26 19:26:32 -0300

    Initial commit :rocket:

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+const task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>To-Do List</h1>
       </header>
+
+      <main>
+        <ul>
+          {task('Jantar')}
+        </ul>
+      </main>
     </div>
   );
 }
